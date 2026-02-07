fa7e5da5265e7051c48e7883557cf189299e3953
Khoi Hoang <khoang.26@pomfret.org>
2026-02-07 14:00:36 -0500

    auto commands

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -1,9 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.ClimbSubsystem;
 
 import java.util.function.BooleanSupplier;
